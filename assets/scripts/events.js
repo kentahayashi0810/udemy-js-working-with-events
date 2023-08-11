a2c978d907571cb05ddce8ffb8f69c2fd59e1f9c
kentahayashi <kenta0810hayashi@gmail.com>
2023-08-11 16:24:28 +0200

    309. Supported Event Types

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,7 +1,17 @@
-const button = document.querySelector("button");
-const buttonClickHandler = () => {
+const buttons = document.querySelectorAll("button");
+
+const buttonClickHandler = (event) => {
   console.log("Button was clicked!");
+  console.log(event);
+  event.target.innerText = "Clicked!";
+  event.target.disabled = true;
+  event.target.style.backgroundColor = "blue";
 };
 // button.onclick = buttonClickHandler;
+buttons.forEach((button) => {
+  button.addEventListener("click", buttonClickHandler);
+});
 
-button.addEventListener("click", buttonClickHandler);
+// setTimeout(() => {
+//   button.removeEventListener("click", buttonClickHandler);
+// }, 2000);
