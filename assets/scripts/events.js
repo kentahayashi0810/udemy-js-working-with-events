323cf63175d9b25506a414b5e3e4930ae8695df8
kentahayashi <kenta0810hayashi@gmail.com>
2023-08-12 12:38:43 +0200

    311. Working with "preventDefault()"

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -8,10 +8,31 @@ const buttonClickHandler = (event) => {
   event.target.style.backgroundColor = "blue";
 };
 // button.onclick = buttonClickHandler;
-buttons.forEach((button) => {
-  button.addEventListener("click", buttonClickHandler);
-});
+// buttons.forEach((button) => {
+//   button.addEventListener("click", buttonClickHandler);
+// });
 
 // setTimeout(() => {
 //   button.removeEventListener("click", buttonClickHandler);
 // }, 2000);
+
+let curElementNumber = 0;
+
+function scrollHandler() {
+  const distanceToBottom = document.body.getBoundingClientRect().bottom;
+
+  if (distanceToBottom < document.documentElement.clientHeight + 150) {
+    const newDataElement = document.createElement("div");
+    curElementNumber++;
+    newDataElement.innerHTML = `<p>Element ${curElementNumber}</p>`;
+    document.body.append(newDataElement);
+  }
+}
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
+
+window.addEventListener("scroll", scrollHandler);
